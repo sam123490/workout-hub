76ec84f74e9942e2bca239f0f619db49b62dc844
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-04-08 12:38:38 -0700

    User model

---
+++ src/main/java/com/samuelrodriguez/workouthub/models/User.java
@@ -0,0 +1,130 @@
+package com.samuelrodriguez.workouthub.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="please enter you first name")
+	@Size(min=2, message="first name must be at least 2 chracters")
+	private String firstName;
+	
+	@NotBlank(message="please enter you last name")
+	@Size(min=2, message="last name must be at least 2 characters")
+	private String lastName;
+	
+	@NotBlank(message="please enter your email")
+	@Email(message="please enter a valid email")
+	private String email;
+	
+	@NotBlank(message="please enter a password")
+	@Size(min=8, message="password must be at least 8 characters")
+	private String password;
+	
+	@Transient
+	@NotBlank(message="please confirm your password")
+	private String confirmPassword;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+	
+	public User() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	@PrePersist
+	public void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	public void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
+}
