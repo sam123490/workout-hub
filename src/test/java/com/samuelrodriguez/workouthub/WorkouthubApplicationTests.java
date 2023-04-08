8bfeeecc644f347bb69bc5f355450b5f0ffa29cb
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-04-08 11:33:03 -0700

    created spring starter project

---
+++ src/test/java/com/samuelrodriguez/workouthub/WorkouthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelrodriguez.workouthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkouthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
